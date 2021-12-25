69d8a0fb2ce8eab763e4ab75b980029ff47218e3
DavidAnimashaun <93924142+DavidAnimashaun@users.noreply.github.com>
2021-12-25 13:27:03 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by user211399 on 12/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+        Text("YESSIR!!!!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ D1App.swift
@@ -0,0 +1,17 @@
+//
+//  D1App.swift
+//  Shared
+//
+//  Created by user211399 on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct D1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
